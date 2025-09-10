a98ae1c887c6d4f8cf07a34866789069b58ed44b
Taha_Afzali <elfo.dp8787@gmail.com>
2025-09-11 01:56:19 +0330

    coding auth funcs

---
+++ script.js
@@ -0,0 +1,13 @@
+const login = (user) => {
+  if (user.name === "taha") {
+    return true;
+  } else {
+    return false;
+  }
+};
+const register = (user) => {
+  return { user, token };
+};
+const getMe = (token) => {
+  return user;
+};
